5e29592de07974c2fb4569fbce59ba44e211fa46
Igor <Igor@DESKTOP-58NU11R>
2025-11-23 13:06:49 +0300

    fixed datetime format for post date

--- SchoolIsComingSoon.Backend/SchoolIsComingSoon.Application/Extensions/DateTimeFormatExtensions.cs
+++ SchoolIsComingSoon.Backend/SchoolIsComingSoon.Application/Extensions/DateTimeFormatExtensions.cs
@@ -1,15 +1,17 @@
-﻿namespace SchoolIsComingSoon.Application.Extensions
+﻿using System.Globalization;
+
+namespace SchoolIsComingSoon.Application.Extensions
 {
     static class DateTimeFormatExtensions
     {
         public static string ToPostFormat(this DateTime dateTime)
         {
-            return dateTime.ToString("dd MMM yyyy").Replace(".", "");
+            return dateTime.ToString("dd MMM yyyy", new CultureInfo("ru-RU")).Replace(".", "");
         }
 
         public static string ToCommentFormat(this DateTime dateTime)
         {
-            return dateTime.ToString("dd MMM yyyy в HH:mm").Replace(".", "");
+            return dateTime.ToString("dd MMM yyyy в HH:mm", new CultureInfo("ru-RU")).Replace(".", "");
         }
     }
 }
